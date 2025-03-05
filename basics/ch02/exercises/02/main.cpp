a6145e2665d6867f5704d943ac452336e0921720
Sergio Sanz <ssanz.ceb@outlook.com>
2025-03-05 20:23:07 +0100

    Added Chapter 2 exercises:
     - Exercise 1
    
    - Added exercise 2
    
    - Added exercise 3

---
+++ basics/ch02/exercises/02/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * This program declares a number of variables
+ * with legal and illegal names to study how
+ * the compiler reacts
+ */
+
+int main ()
+{
+      int double = 0; // illegal
+      double This_little_pig; // legal
+      int 2_for_1_special; // illegal
+      char $PATH; //legal
+      char correct?; // Illegal
+}
