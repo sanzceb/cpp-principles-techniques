247c12f61388611f8fcd70e966e19b121f4f8c7a
Sergio Sanz <ssanz.ceb@outlook.com>
2025-03-08 20:00:08 +0100

    drill exercise 4

--- basics/ch03/drill/drill.cpp
+++ basics/ch03/drill/drill.cpp
@@ -1,4 +1,4 @@
-// A program that consists of a while-loop that reads two integers each time
+// A program that consists of a while-loop that reads two doubles each time
 // then prints which is the smaller and which is the larger value. Otherwise
 // it prints that both numbers are equal
 
@@ -7,20 +7,22 @@
 int main()
 {
       cout << "Enter a sequence of numbers in pairs and then enter \"|\": ";
-      vector<int> temps;
-      int temp;
+      int i = 0;
+      double temp;
+      double prev; // the previous number
       while (cin >> temp) {
-	    temps.push_back(temp);
-	    int i = temps.size();
-	    if (i % 2 == 0) // test we print every two iterations
-		  if (temps[i-2]< temps[i-1])
-			cout << "The smaller value is: " << temps[i-2] << '\n'
-			      << "The larger value is: " << temps[i-1] << '\n';
-		  else if (temps[i-2] > temps[i-1])
-			cout << "The smaller value is: " << temps[i-1] << '\n'
-			      << "The larger value is: " << temps[i-2] << '\n';
+	    if (i % 2) // is temp the second value?
+		  if (temp < prev)
+			cout << "The smaller value is: " << temp << '\n'
+			      << "The larger value is: " << prev << '\n';
+		  else if (temp > prev)
+			cout << "The smaller value is: " << prev << '\n'
+			      << "The larger value is: " << temp << '\n';
 		  else
 			cout << "The numbers are equal\n";
 			
+	    else
+		  prev = temp;
+	    ++i;
       }
 }
