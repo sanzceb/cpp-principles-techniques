0cc90c3b95aedfdc4740c7a56a85a2c57ffbb094
Sergio Sanz <ssanz.ceb@outlook.com>
2025-03-08 18:42:50 +0100

    drill exercise 3

--- basics/ch03/drill/drill.cpp
+++ basics/ch03/drill/drill.cpp
@@ -1,5 +1,6 @@
 // A program that consists of a while-loop that reads two integers each time
-// then prints which is the smaller and which is the larger value
+// then prints which is the smaller and which is the larger value. Otherwise
+// it prints that both numbers are equal
 
 #include "../PPPheaders.h"
 
@@ -11,13 +12,15 @@ int main()
       while (cin >> temp) {
 	    temps.push_back(temp);
 	    int i = temps.size();
-	    if (i % 2 == 0)
+	    if (i % 2 == 0) // test we print every two iterations
 		  if (temps[i-2]< temps[i-1])
 			cout << "The smaller value is: " << temps[i-2] << '\n'
 			      << "The larger value is: " << temps[i-1] << '\n';
-		  else
+		  else if (temps[i-2] > temps[i-1])
 			cout << "The smaller value is: " << temps[i-1] << '\n'
 			      << "The larger value is: " << temps[i-2] << '\n';
+		  else
+			cout << "The numbers are equal\n";
 			
       }
 }
