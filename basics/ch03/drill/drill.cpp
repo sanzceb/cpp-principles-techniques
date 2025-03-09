b0f2a0c75e00384dc79ee3b0d2335095ed2c2e20
Sergio Sanz <ssanz.ceb@outlook.com>
2025-03-09 10:31:51 +0100

    drill number 8

--- basics/ch03/drill/drill.cpp
+++ basics/ch03/drill/drill.cpp
@@ -1,26 +1,42 @@
-// A program that reads a sequence of doubles and keeps track of the
-// biggest and the smallest value at any given time
+// A program that reads a sequence of values and units
+// and keeps track of the biggest and the smallest value at any given time
 
 #include "../PPPheaders.h"
 
+double value_as_m (double value, string unit)
+      // Search the appropriate factor to convert into meters
+      // from other units. If the unit argument is not found
+      // no conversion is made
+{
+      vector<string> units = {"m", "in", "ft", "cm"};
+      vector<double> factors = {1, 0.0254, 0.3048, 0.01}; // conversion factors
+      for (int i=0;i < units.size(); ++i)
+	    if (units[i] == unit)
+		  return factors[i] * value;
+      return value;
+}
+
 int main()
 {
-      double min; // Keep track of the smallest value
-      double max; // Keep track of the biggest value at each time
+      // The first number is always the biggest
       double temp;
+      string unit; 
       cout << "Enter a sequence of numbers in pairs and then enter \"|\": ";
-      cin >> temp; // The first number is always the biggest
-      cout << temp << " the biggest so far" << '\n'; 
-      min = temp;
-      max = temp; 
-      while (cin >> temp) 
-	    if (temp < min) {
-		  min = temp;
-		  cout << temp << " the smallest so far" << '\n';
-	    } else if (temp > max) {
-		  max = temp;
-		  cout << temp << " the biggest so far" << '\n';
+      cin >> temp >> unit; 
+      cout << temp << unit << " the biggest so far" << '\n'; 
+
+      double min_m = value_as_m(temp, unit); // track the smallest value
+      double max_m = min_m; // track the biggest value
+      while (cin >> temp >> unit) {
+	    double temp_m = value_as_m(temp, unit);
+	    if (temp_m < min_m) {
+		  min_m = temp_m;
+		  cout << temp << unit << " the smallest so far" << '\n';
+	    } else if (temp_m > max_m) {
+		  max_m = temp_m;
+		  cout << temp << unit << " the biggest so far" << '\n';
 	    } else
-		  cout << temp << '\n';
+		  cout << temp << unit << '\n';
+      }
 }
 
