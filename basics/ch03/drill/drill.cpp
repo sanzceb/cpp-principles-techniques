2ceb0dba86f98998f7710c502c0990b1a378d888
Sergio Sanz <ssanz.ceb@outlook.com>
2025-03-09 11:18:44 +0100

    drill exercise 8: reject illegal units

--- basics/ch03/drill/drill.cpp
+++ basics/ch03/drill/drill.cpp
@@ -6,14 +6,14 @@
 double value_as_m (double value, string unit)
       // Search the appropriate factor to convert into meters
       // from other units. If the unit argument is not found
-      // no conversion is made
+      // no conversion is made and -1 is returned
 {
       vector<string> units = {"m", "in", "ft", "cm"};
       vector<double> factors = {1, 0.0254, 0.3048, 0.01}; // conversion factors
       for (int i=0;i < units.size(); ++i)
 	    if (units[i] == unit)
 		  return factors[i] * value;
-      return value;
+      return -1;
 }
 
 int main()
@@ -29,7 +29,10 @@ int main()
       double max_m = min_m; // track the biggest value
       while (cin >> temp >> unit) {
 	    double temp_m = value_as_m(temp, unit);
-	    if (temp_m < min_m) {
+	    if (temp_m == -1) {
+		  cout << "Sorry, I do not know a unit called "
+			<< unit << '\n';
+	    } else if (temp_m < min_m) {
 		  min_m = temp_m;
 		  cout << temp << unit << " the smallest so far" << '\n';
 	    } else if (temp_m > max_m) {
